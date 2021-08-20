091558cd284ce77008dd58fbec7aaec78f590e11
huynhquanghao98 <huynhquanghao98@gmail.com>
2021-08-20 19:14:47 +0700

    HaoHQ_add_fontend

---
+++ frontend/connect.php
@@ -0,0 +1,18 @@
+<?php 
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "shop";
+	
+	// Create connection
+	// Example (MySQLi Object-Oriented)
+	$con = new mysqli($servername, $username, $password, $database);
+
+	// Check connection
+	if ($con->connect_error) {
+	    die("Connection failed: " . $con->connect_error);
+	} else {
+		echo "Connected successfully";
+	}
+	
+?>
